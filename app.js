60198d9d820aa4989487625bedb18f6d20212fad
HIFZA EMAAN <hifza.4.106023@gmail.com>
2025-12-09 20:57:05 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,68 @@
+//******* chapter 6-9 question-********
+//var a = 10;
+
+//document.write("VALUE OF A IS 10:<br> <hr>");
+//document.write("Result:<br>");
+//document.write("The value of a is: " + a + "<br><br>");
+
+//document.write("The value of ++a is: " + (++a) + "<br>");
+//document.write("Now the value of a is: " + a + "<br><br>");
+
+//document.write("The value of a++ is: " + (a++) + "<br>");
+//document.write("Now the value of a is: " + a + "<br><br>");
+
+// document.write("The value of --a is: " + (--a) + "<br>");
+//document.write("Now the value of a is: " + a + "<br><br>");
+
+//document.write("The value of a-- is: " + (a--) + "<br>");
+//document.write("Now the value of a is: " + a + "<br><hr>");
+          //---------------//
+ //******chapter 6-9(question-2)*******
+
+//document.write("<h1>QUESTION 2:</h1> <hr>");
+//var a = 2, b = 1;
+//var result = --a - --b + ++b + b--;
+
+//var tableHTML = "<table>";
+//tableHTML += "<tr><th>Variable</th><th>Value</th></tr>";
+//tableHTML += "<tr><td>a</td><td>" + a + "</td></tr>";
+//tableHTML += "<tr><td>b</td><td>" + b + "</td></tr>";
+//tableHTML += "<tr><td>Result</td><td>" + result + "</td></tr>";
+//tableHTML += "</table>";
+
+//document.getElementById("resultTable").innerHTML = tableHTML;
+
+ //******chapter 6-9(question-3)*******
+
+
+ //var greeting = prompt("enter your name");
+// document.write("Hi the best! "+ greeting);
+
+ //******chapter 6-9(question-5)*******
+
+// var table = +prompt("enter a number") ;
+
+//  if (table == ""){
+  //    table = 5; }
+
+// for (var i = 1; i <= 10; i++) {
+     //document.write(table + " x " + i + " = " + table * i + "<br>"); }
+
+
+ //******chapter 6-9(question-6)*****
+
+
+ //var sub1 = prompt("Enter Subject 1");
+// var marksObtain1 = +prompt("Enter marks obtained in subject 1 out of 100");
+// var sub2 = prompt("Enter Subject 2");
+//var marksObtain2 = +prompt("Enter marks obtained in subject 2 out of 100");
+// var sub3 = prompt("Enter Subject 3");
+// var marksObtain3 = +prompt("Enter marks obtained in subject 3 out of 100");
+//var totalMarks = 100;
+// var sub1Per = marksObtain1 / totalMarks * 100;
+//  var sub2Per = marksObtain2 / totalMarks * 100;
+// var sub3Per = marksObtain3 / totalMarks * 100;
+// var totalObtMarks = marksObtain1 + marksObtain2 + marksObtain3;
+// document.write("<table><tr><th> Subject </th><th> Total Marks </th><th> Obtained Marks </th><hr><th>Percentage</th></tr> <tr><td>"+sub1+"</td><td>" + totalMarks + "</td><td>" + marksObtain1 + "</td><td>" + sub1Per +"% </td></tr> <tr><td>" + sub2 + "</td><td>" + totalMarks + "</td><td>" + marksObtain2 + "</td><td>" + sub2Per +"% </td></tr> <tr><td>" + sub3 + "</td><td>" + totalMarks + "</td><td>" + marksObtain3 + "</td><td>" + sub3Per +"% </td></tr> <tr><td></td><th>"+ totalMarks*3 +"</th><th>"+ totalObtMarks +"</th><th>"+(totalObtMarks/(totalMarks*3 )*100).toFixed(1)+"% </th></tr><hr> </table>");
+//------THE END -------//
+//----- IT'S THE END -----//
