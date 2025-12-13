7b8cbab10807751315f83609906606cd967c519c
HIFZA EMAAN <hifza.4.106023@gmail.com>
2025-12-13 16:14:47 +0500

    Comment out multiplication table code
    Comment out code related to multiplication table generation.

--- app.js
+++ app.js
@@ -40,13 +40,18 @@
 
  //******chapter 6-9(question-5)*******
 
-// var table = +prompt("enter a number") ;
+// var table = +prompt("Enter your fav number?");
 
-//  if (table == ""){
-  //    table = 5; }
-
-// for (var i = 1; i <= 10; i++) {
-     //document.write(table + " x " + i + " = " + table * i + "<br>"); }
+//document.write( table + " * 1 = " + table * 1 + "<br />")
+//document.write( table + " * 2 = " + table * 2 + "<br />")
+//document.write( table + " * 3 = " + table * 3 + "<br />")
+//document.write( table + " * 4 = " + table * 4 + "<br />")
+//document.write( table + " * 5 = " + table * 5 + "<br />")
+//document.write(table + " * 6 = " + table * 6 + "<br />")
+//document.write(table + " * 7 = " + table * 7 + "<br />")
+//document.write(table + " * 8 = " + table * 8 + "<br />")
+//document.write(table + " * 9 = " + table * 9 + "<br />")
+//document.write( table + " * 10 = " + table * 10)
 
 
  //******chapter 6-9(question-6)*****
